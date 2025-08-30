f06b70c3f57fe7ed42c4951faff11a274df1e99d
古垣　博光 <h.f.appmaker@gmail.com>
2025-08-30 22:36:32 +0900

    Revert "feat: Remove edit button from contract details"
    This reverts commit e7488fc28f8b28aa9fcc03a2f783e0d2808f788c.

--- src/app/(protected)/contracts/page.client.tsx
+++ src/app/(protected)/contracts/page.client.tsx
@@ -597,6 +597,14 @@ export default function ContractsClientPage({ userId }: { userId: string }) {
 
             <DialogFooter sticky className="p-6">
               <Button
+                variant="outline"
+                onClick={() => {
+                  setActiveDialog("edit");
+                }}
+              >
+                編集
+              </Button>
+              <Button
                 variant="destructive"
                 onClick={() => {
                   setActiveDialog("delete");
